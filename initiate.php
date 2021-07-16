16b5aeaf68f3d8e102363b47e34838d435e8f324
Matthias Palmer <matthias@metasolutions.se>
2021-07-16 17:40:57 +0200

    Challenge with QR-code

--- initiate.php
+++ initiate.php
@@ -10,8 +10,12 @@ $bankApp  = 'swedbank_foretag';
 $username = $_GET['pnr'];
 
 $appData = new SwedbankJson\AppData($bankApp, __DIR__.'/AppData.json');
-$auth = new SwedbankJson\Auth\MobileBankID($appData, $username);
-$auth->initAuth();
+//print_r(__DIR__.'/AppData.json');
+//SwedbankJson\AppData::test();
+//print_r('Type: '.gettype($appData));
 
-header('Content-type: application/json');
-print_r(json_encode(array('status' => 'initiate'), JSON_PRETTY_PRINT));
+
+$auth = new SwedbankJson\Auth\MobileBankID($appData, $username);
+$output = $auth->initAuth();
+header('Content-type: image/png');
+print_r($auth->QRCode());
